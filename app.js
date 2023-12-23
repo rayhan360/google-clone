23c7b48cafd9dcd5348e077ba27ba6ff817a3ec2
rayhan360 <hossainrayhan897@gmail.com>
2023-12-24 03:45:33 +0600

    added heading gmail and image search navigation and design

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const toggleMenu = () => {
-    const dropdown = document.getElementById('myDropdown');
-    dropdown.style.display = (dropdown.style.display === 'none' || dropdown.style.display === '') ? 'block' : 'none'
-}
+  const dropdown = document.getElementById("myDropdown");
+  dropdown.style.display =
+    dropdown.style.display === "none" || dropdown.style.display === ""
+      ? "block"
+      : "none";
+};
 
 const googleSearch = (e) => {
-    e.preventDefault();
-    const prompt = document.getElementById("search").value;
-    if (prompt === "") {
-        alert("Please enter a search query.");
-        return;
-    }
-    var query = prompt.replace(" ", "+", prompt);
-    var url = "http://www.google.com/search?q=" + query;
+  e.preventDefault();
+  const prompt = document.getElementById("search").value;
+  if (prompt === "") {
+    alert("Please enter a search query.");
+    return;
+  }
+  var query = prompt.replace(" ", "+", prompt);
+  var url = "http://www.google.com/search?q=" + query;
 
-    window.location.href = url;
+  window.location.href = url;
 };
